ada1cabfbaa31365cc3452ba4c7c7f26aad77052
seriouskang <scdean110@gmail.com>
2023-01-07 22:32:38 +0900

    ChunkOrientedTasklet concept and basic API

--- spring_batch_based_on_spring_boot/springbatch/src/main/java/io/study/springbatch/ChunkOrientedTaskletConfiguration.java
+++ spring_batch_based_on_spring_boot/springbatch/src/main/java/io/study/springbatch/ChunkOrientedTaskletConfiguration.java
@@ -5,19 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class ChunkConfiguration {
+public class ChunkOrientedTaskletConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
@@ -30,21 +33,24 @@ public class ChunkConfiguration {
     }
 
     @Bean
+    @JobScope
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<String, String>chunk(5)
-                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
+                .<String, String>chunk(4)
+                .reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5", "item6")))
                 .processor(new ItemProcessor<String, String>() {
                     @Override
                     public String process(String item) throws Exception {
-                        Thread.sleep(3000);
-                        log.info("item = {}", item);
-                        return String.format("my %s", item);
+                        Thread.sleep(1000);
+                        return "my_" + item;
                     }
                 })
-                .writer(items -> {
-                    Thread.sleep(3000);
-                    log.info("items = {}", items);
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        log.info("----------------");
+                        items.forEach(item -> log.info("item = {}", item));
+                    }
                 })
                 .build();
     }
